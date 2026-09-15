4925bc37cea48ecd5c72f3a238e2673eefa4868b
PedroEdur <145567971+PedroEdur@users.noreply.github.com>
2026-09-15 08:20:44 -0300

    movimet player

--- PongMult/Assets/Scripts/PlayerController.cs
+++ PongMult/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float velocidade = 5f;
 
-    // Update is called once per frame
+    public bool jogador1;
+
     void Update()
     {
-        
+        float movimento = 0f;
+
+        if (jogador1)
+        {
+            if (Input.GetKey(KeyCode.W))
+                movimento = 1f;
+
+            if (Input.GetKey(KeyCode.S))
+                movimento = -1f;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                movimento = 1f;
+
+            if (Input.GetKey(KeyCode.DownArrow))
+                movimento = -1f;
+        }
+
+        transform.position += Vector3.up * movimento * velocidade * Time.deltaTime;
+
+        float y = Mathf.Clamp(transform.position.y, -3.5f, 3.5f);
+
+        transform.position = new Vector3(
+            transform.position.x,
+            y,
+            transform.position.z
+        );
     }
 }
